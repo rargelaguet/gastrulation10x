70b67b0c515c98c2c3164ca315049df5761514c3
rargelaguet <ricard.argelaguet@gmail.com>
2020-06-04 17:19:29 +0200

    update iterative mapping

--- iterative_mapping/mnn/iterative_mnn.R
+++ iterative_mapping/mnn/iterative_mnn.R
@@ -20,9 +20,11 @@ if (grepl("ricard",Sys.info()['nodename'])) {
   source("/Users/ricard/gastrulation10x/settings.R")
   source("/Users/ricard/gastrulation10x/iterative_mapping/mnn/utils.R")
 } else if(grepl("ebi",Sys.info()['nodename'])){
-  stop()
+  source("/homes/ricard/gastrulation10x/settings.R")
+  source("/homes/ricard/gastrulation10x/iterative_mapping/mnn/utils.R")
 }  
-io$outfile <- "/Users/ricard/data/gastrulation10x/results/iterative_mapping/mnn/test.txt"
+io$outfile <- paste0(io$basedir,"/results/iterative_mapping/mnn/test.txt")
+io$dist <- paste0(io$basedir,"/results/phylogenetic_tree/PAGA_distances.csv.gz")
 dir.create(io$outdir, showWarnings = F)
 
 # Cell types to use
@@ -33,29 +35,25 @@ opts$celltypes <- c(
   "ExE ectoderm",
   "Notochord"
 )
+# opts$celltypes <- opts$celltypes %>% stringr::str_replace_all("/", "_") %>% stringr::str_replace_all("_", " ") 
 
 # Test mode
 opts$test_mode <- TRUE
 
-# Maximum of M marker genes per cell type (sorted according to marker score)
-# opts$max.genes <- 50
-
 # Update metadata
 sample_metadata <- sample_metadata %>%
+  .[,celltype:=stringr::str_replace_all(celltype,"/", "_")] %>%
   .[,celltype:=stringr::str_replace_all(celltype,"_", " ")] %>%
   .[celltype%in%opts$celltypes]
 
+sample_metadata_atlas <- sample_metadata %>% .[stage%in%c("E7.0","E7.25","E7.75","E8.0")]
+sample_metadata_query <- sample_metadata %>% .[stage=="E7.5"]
+  
 # Subset cells
-# if (isTRUE(opts$test_mode)) {
-sample_metadata_query <- sample_metadata %>% 
-  split(.$celltype) %>% map(~ head(.,n=500)) %>% rbindlist
-sample_metadata_atlas <- sample_metadata %>% 
-  split(.$celltype) %>% map(~ tail(.,n=500)) %>% rbindlist
-# } else {
-#   sample_metadata_query <- sample_metadata
-# }
-
-# table(sample_metadata$celltype)
+if (isTRUE(opts$test_mode)) {
+  sample_metadata_query <- sample_metadata %>% split(.$celltype) %>% map(~ head(.,n=50)) %>% rbindlist
+  sample_metadata_atlas <- sample_metadata %>% split(.$celltype) %>% map(~ tail(.,n=50)) %>% rbindlist
+}
 
 # Load RNA expression data
 sce <- readRDS(io$rna.sce)
@@ -81,12 +79,9 @@ dim(sce.atlas)
 ## Define distance matrix for hierarchical clustering ##
 ########################################################
 
-io$dist <- "/Users/ricard/data/gastrulation10x/results/phylogenetic_tree/PAGA_distances.csv.gz"
 dist <- fread(io$dist) %>%
   as.data.frame %>% tibble::column_to_rownames("V1") %>% as.matrix %>%
   .[opts$celltypes,opts$celltypes] %>% as.dist
-
-plot(hclust(dist))
 
 #######################
 ## Recursive mapping ##
@@ -105,17 +100,18 @@ while (any(grepl("%",sce.query$pred.celltype))) {
 ## Fetch statistics ##
 ######################
 
-foo <- data.table(cell=colnames(sce.query), celltype.pred = sce.query$pred.celltype)
+pred.dt <- data.table(cell=colnames(sce.query), celltype.pred = sce.query$pred.celltype) %>%
+  merge(sample_metadata_query[,c("cell","celltype")])
+mean(pred.dt$celltype==pred.dt$celltype.pred)
 
-foobar <- merge(sample_metadata_query[,c("cell","celltype")], foo, by="cell")
-mean(foobar$celltype==foobar$celltype.pred)
+##########
+## Plot ##
+##########
 
-to.plot <- foobar[,.N, by=c("celltype","celltype.pred")] %>%
+to.plot <- pred.dt[,.N, by=c("celltype","celltype.pred")] %>%
   .[,total:=sum(N),by="celltype"] %>% .[,fraction:=N/total] %>% .[,total:=NULL]
-  #dcast(celltype~celltype.pred, value.var="N")# %>%
-  # matrix.please
 
-ggplot(to.plot, aes(x=celltype, y=celltype.pred, fill=fraction)) +
+p <- ggplot(to.plot, aes(x=celltype, y=celltype.pred, fill=fraction)) +
   geom_tile(color="black") +
   geom_text(aes(label=fraction)) +
   theme_classic() +
@@ -126,7 +122,13 @@ ggplot(to.plot, aes(x=celltype, y=celltype.pred, fill=fraction)) +
     axis.text.y = element_text(color="black",size=rel(0.8)),
     axis.text.x = element_text(colour="black",size=rel(0.8), angle=90, hjust=1, vjust=0.5)
   )
-# pheatmap::pheatmap(to.plot, cluster_rows=F, cluster_cols=F)
 
-# Save cell type predictions
-fwrite(foobar, io$outfile, sep="\t")
+pdf(paste0(io$outdir,"/foo.pdf"), width=8, height=6.5, useDingbats = F)
+print(p)
+dev.off()
+
+##########
+## Save ##
+##########
+
+fwrite(pred.dt, io$outfile, sep="\t")
