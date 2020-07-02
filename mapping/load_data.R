44b5742b51109fc302fa3bc17149994625432abb
rargelaguet <ricard.argelaguet@gmail.com>
2020-07-02 21:13:46 +0200

    add mnn mapping

---
+++ mapping/load_data.R
@@ -0,0 +1,65 @@
+###################
+## Load metadata ##
+###################
+
+# opts$celltypes <- opts$celltypes %>% stringr::str_replace_all("/", "_") %>% stringr::str_replace_all("_", " ") 
+
+# Update metadata
+sample_metadata <- sample_metadata %>%
+  .[,celltype:=stringr::str_replace_all(celltype,"/", "_")] %>%
+  .[,celltype:=stringr::str_replace_all(celltype,"_", " ")] %>%
+  .[stage%in%args$stages & celltype%in%args$celltypes]
+
+metadata_query <- sample_metadata %>% .[sample%in%as.character(args$test_samples)]
+metadata_atlas <- sample_metadata %>% .[!sample%in%as.character(args$test_samples)]
+  
+# Subset cells
+if (isTRUE(args$test)) {
+  metadata_query <- metadata_query %>% split(.$celltype) %>% map(~ head(.,n=50)) %>% rbindlist
+  metadata_atlas <- metadata_atlas %>% split(.$celltype) %>% map(~ tail(.,n=50)) %>% rbindlist
+}
+
+###############
+## Load data ##
+###############
+
+# Load SingleCellExperiment
+sce <- readRDS(io$rna.sce)
+
+# remove lowly expressed genes
+sce <- sce[rowSums(counts(sce))>10,]
+
+# Define query
+sce.query <- sce[,metadata_query$cell]
+colData(sce.query) <- metadata_query %>% tibble::column_to_rownames("cell") %>% DataFrame
+
+# Define atlas
+sce.atlas <- sce[,metadata_atlas$cell]
+colData(sce.atlas) <- metadata_atlas %>% tibble::column_to_rownames("cell") %>% DataFrame
+
+rm(sce)
+
+#################
+## Subset HVGs ##
+#################
+
+# Select HVGs
+# hvg <- getHVGs(sce.atlas, block=as.factor(sce.atlas$sample), p.value = 0.10)
+# sce.query <- sce.query[hvg,]
+# sce.atlas <- sce.atlas[hvg,]
+
+#########################
+## Subset marker genes ##
+#########################
+
+# # Load gene markers to be used as HVGs
+# marker_genes.dt <- fread(io$marker_genes)
+# marker_genes.dt <- marker_genes.dt[,head(.SD,n=50),by="celltype"]
+# marker_genes <- unique(marker_genes.dt$ens_id)
+# 
+# stopifnot(all(marker_genes%in%rownames(sce.atlas)))
+# stopifnot(all(marker_genes%in%rownames(sce.query)))
+# 
+# # Update SingleCellExperiment objects
+# sce.query <- sce.query[marker_genes,]
+# sce.atlas <- sce.atlas[marker_genes,]
---
+++ mapping/run.R
@@ -0,0 +1,131 @@
+suppressPackageStartupMessages(library(SingleCellExperiment))
+suppressPackageStartupMessages(library(scran))
+suppressPackageStartupMessages(library(scater))
+suppressPackageStartupMessages(library(batchelor))
+suppressPackageStartupMessages(library(edgeR))
+suppressPackageStartupMessages(library(argparse))
+
+######################
+## Define arguments ##
+######################
+
+p <- ArgumentParser(description='')
+p$add_argument('--stages',        type="character",   nargs='+',  help='Atlas stage(s)')
+p$add_argument('--celltypes',        type="character",   nargs='+',  help='Atlas cell types(s)')
+p$add_argument('--test_samples',  type="character",   nargs='+',  help='Query batch(es)')
+p$add_argument('--test',          action = "store_true",  help = 'Testing mode')
+args <- p$parse_args(commandArgs(TRUE))
+
+## START TEST ##
+args$stages <- c(
+  "E6.5",
+  "E6.75",
+  "E7.0",
+  "E7.25",
+  "E7.5"
+  # "E7.75"
+  # "E8.0",
+  # "E8.25",
+  # "E8.5"
+  # "mixed_gastrulation"
+)
+
+args$celltypes = c(
+  "Epiblast"
+  # "Primitive_Streak",
+  # "Caudal_epiblast",
+  # "PGC",
+  # "Anterior_Primitive_Streak",
+  # "Notochord",
+  # "Def._endoderm",
+  # "Gut",
+  # "Nascent_mesoderm",
+  # "Mixed_mesoderm",
+  # "Intermediate_mesoderm",
+  # "Caudal_Mesoderm",
+  # "Paraxial_mesoderm",
+  # "Somitic_mesoderm",
+  # "Pharyngeal_mesoderm",
+  # "Cardiomyocytes",
+  # "Allantois",
+  # "ExE_mesoderm",
+  # "Mesenchyme",
+  # "Haematoendothelial_progenitors",
+  # "Endothelium",
+  # "Blood_progenitors_1",
+  # "Blood_progenitors_2",
+  # "Erythroid1",
+  # "Erythroid2",
+  # "Erythroid3",
+  # "NMP",
+  # "Rostral_neurectoderm",
+  # "Caudal_neurectoderm",
+  # "Neural_crest",
+  # "Forebrain_Midbrain_Hindbrain",
+  # "Spinal_cord",
+  # "Surface_ectoderm",
+  # "Visceral_endoderm",
+  # "ExE_endoderm",
+  # "ExE_ectoderm",
+  # "Parietal_endoderm"
+)
+
+args$test_samples <- c(
+  # E7.5
+  # "2",
+  # "3",
+  "4",
+  "6"
+  # "19",
+  # "20"
+)
+args$test <- FALSE
+## END TEST ##
+
+#####################
+## Define settings ##
+#####################
+
+# Load default settings
+if (grepl("ricard",Sys.info()['nodename'])) {
+  source("/Users/ricard/gastrulation10x/settings.R")
+  source("/Users/ricard/gastrulation10x/mapping/stages/utils.R")
+  io$load_data_script <- "/Users/ricard/gastrulation10x/mapping/load_data.R"
+} else if(grepl("ebi",Sys.info()['nodename'])){
+  source("/homes/ricard/gastrulation10x/settings.R")
+  source("/homes/ricard/gastrulation10x/mapping/stages/utils.R")
+  io$load_data_script <- "/homes/ricard/gastrulation10x/mapping/load_data.R"
+}  
+# io$marker_genes <- paste0(io$basedir,"/results/marker_genes/all_stages/marker_genes.txt.gz")
+io$outdir <- paste0(io$basedir,"/results/mapping/stages")
+
+if (isTRUE(args$test)) print("Test mode activated...")
+
+###############
+## Load data ##
+###############
+
+source(io$load_data_script)
+
+#############
+## Run MNN ##
+#############
+
+# Joint normalisation
+sce.all <- joint.normalisation(sce.query, sce.atlas, cosineNorm = FALSE)
+
+# Select HVGs
+# genes <- getHVGs(sce.atlas, block=as.factor(sce.atlas$sample), p.value = 0.10)
+genes <- getHVGs(sce.all, block=sce.all$block, p.value = 0.05)
+
+# MNN
+mapping.dt <- mnn.fn(sce.all, sce.query, sce.atlas, genes = genes, npcs = 25, k = 15)
+
+# table(mapping.dt$celltype_mapped)
+
+##########
+## Save ##
+##########
+
+fwrite(mapping.dt, sprintf("%s/mapping_mnn_%s.txt.gz",io$outdir,paste(args$test_samples,collapse="_")), sep="\t")
+
